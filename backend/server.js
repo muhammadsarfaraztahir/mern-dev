ed134efcc84850c3cd083552e581ca36bf10d378
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-01-21 10:31:38 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-mongoose.connect("")
+mongoose.connect(process.env.URI)
+.then(()=>{
+    console.log("connecting successfully");
+    app.listen(process.env.PORT || 8000), (err) => {
+        if(err) console.log(err);
+        console.log("running successfully at", process.env.PORT);
+    }
+})
+.catch((error)=>{
+    console.log("error", error);    
+})
 
 
 app.get("/", (req, res) => {
